a2d6d71a0d435c416f693e59354102fa50992102
DESKTOP-G72575G\LINH DEP TRAI <nnlinh97it@gmail.com>
2018-04-15 22:28:43 +0700

    completed index

--- bookstore/assets/js/main.js
+++ bookstore/assets/js/main.js
@@ -99,6 +99,33 @@
       },
     ]
   });
+  $('#product-slick-3').slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    dots: true,
+    arrows: false,
+    appendDots: '.product-slick-dots-1',
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 
   // PRODUCT DETAILS SLICK
   $('#product-main-view').slick({
