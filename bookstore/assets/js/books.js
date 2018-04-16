d54fa5188b9b55c69f452864e5a3a2d542c38306
Cao Gia Huy <thegia.huy102@gmail.com>
2018-04-16 20:24:41 +0700

    - Cập nhật productstemplate.html: Hiển thị danh sách sản phẩm bằng Jquery. - Thêm productstemplate.js: file .js thực hiện tìm kiếm sản phẩm có tên nhà sản xuất tương ứng rồi thêm vào .html. - Thêm book.js: file .js chứa dữ liệu các sản phẩm.

---
+++ bookstore/assets/js/books.js
@@ -0,0 +1,13 @@
+var item = [];
+var imgUrl = "../../assets/img/books/";
+
+// Link ảnh, tên sách, giá , giá cũ, rate (sao), tình trạng (new), sale, nhà xuất bản, loại sách, view, số lượng bán
+item.push(["01.jpg","Không Gia Đình (Ấn Bản Kỉ Niệm 60 Năm Thành Lập NXB Kim Đồng)",74000,110000,5,"",32, "Kim Đồng", "Văn Học",0,0]);
+item.push(["02.jpg","33 Bài Thực Hành Theo Phương Pháp Shichida",37950,55000,4,"",31, "Kim Đồng", "Khác",0,0]);
+item.push(["03.jpg","Truyện Kể Về Danh Nhân Thế Giới - Ludwig Van Beethoven",31050,45000,4,"",31, "Kim Đồng", "Văn Học",0,0]);
+item.push(["04.jpg","Danh Nhân Thế Giới - Napôlêông (2014)",17500,25000,4,"",30, "Kim Đồng", "Văn Học",0,0]);
+item.push(["05.jpg","Danh Nhân Thế Giới - Anhxtanh (2014)",17500,25000,4,"",30, "Kim Đồng", "Văn Học",0,0]);
+item.push(["06.jpg","Mẹ Do Thái Dạy Con Tư Duy",38760,57000,4,"",32, "Kim Đồng", "Khác",0,0]);
+item.push(["07.jpg","Cẩm Nang Sinh Hoạt Bằng Tranh Cho Bé - Kĩ Năng Trong Sinh Hoạt Thường Ngày",21440,32000,4,"",33, "Kim Đồng", "Khác",0,0]);
+item.push(["08.jpg","Pony Kí Sự - Hiểu Để Yêu Con Trọn Vẹn - Làm Bạn Với Con Từ 1-3 Tuổi",24150,35000,4,"",31, "Kim Đồng", "Khác",0,0]);
+item.push(["09.jpg","Cánh Buồm Đỏ Thắm (Tái Bản 2014)",14520,22000,4,"",34, "Kim Đồng", "Văn Học",0,0]);
---
+++ bookstore/assets/js/productstemplate.js
@@ -0,0 +1,92 @@
+$(function(){
+
+
+   // Link ảnh, tên sách, giá , giá cũ, rate (sao), tình trạng (new), sale, nhà xuất bản, loại sách, view, số lượng bán
+   var nxb = "Kim Đồng";
+
+   // Thêm tên nhà xuất bản vào breadcrumb
+   $("#breadcrumb .container .breadcrumb").append("<li class='active'>"+nxb+"</li>");
+
+   $.each(item,function(index){
+   	if (item[index][7]===nxb){
+	   	var divStr = "";
+	   	divStr += "\
+		   	<div class='col-md-4 col-sm-6 col-xs-6'>\
+				<div class='product product-single'>\
+					<div class='product-thumb'>\
+						<div class='product-label'>\
+		";
+
+		// Thêm vào tình trạng nếu có
+		if(item[index][5]!==""){
+			divStr+="\
+							<span>"+item[index][5]+"</span>\
+			";
+		}
+
+		// Thêm vào sale nếu có
+		if(item[index][6]!==0){
+			divStr+="\
+							<span class='sale'>-"+item[index][6]+"%</span>\
+			";
+		}
+
+		// Thêm vào nút QuickView
+		divStr+="\
+		   				</div>\
+						<button class='main-btn quick-view'><i class='fa fa-search-plus'></i> View</button>\
+						<img src='"+imgUrl+item[index][0]+"' alt=''>\
+					</div>\
+					<div class='product-body'>\
+		";
+
+		// Nếu có giảm giá thì hiển thị giá mới và giá cũ, không giảm giá thì chỉ hiển thị giá
+		if(item[index][3]!==0){
+			divStr+="\
+						<h3 class='product-price'><a>"+item[index][2]+"đ </a><del class='product-old-price'>"+item[index][3]+"đ</del></h3>\
+			";
+		} else {
+			divStr+="\
+						<h3 class='product-price'><a>"+item[index][2]+"đ </a></h3>\
+			";
+		}
+
+		// Thêm vào rating (ngôi sao)
+		divStr+="\
+		   				<div class='product-rating'>\
+		";
+
+		// Chạy vòng lặp tạo sao
+		var i;
+		for (i=0; i<5; i++) {
+			if (i<item[index][4]) {
+				divStr+="\
+		   				<i class='fa fa-star'></i>\
+				";
+			} else {
+				divStr+="\
+		   				<i class='fa fa-star-o empty'></i>\
+				";
+			}
+		}
+
+		// Thêm vào tên của sách, các nút phía dưới và đóng lại thẻ <div>
+		divStr+="\
+					</div>\
+		 			<h2 class='product-name'><a href='#''>"+item[index][1]+"</a></h2>\
+		 			<div class='product-btns'>\
+		 				<button class='main-btn icon-btn'><i class='fa fa-heart'></i></button>\
+		 				<button class='main-btn icon-btn'><i class='fa fa-exchange'></i></button>\
+		 				<button class='primary-btn add-to-cart'><i class='fa fa-shopping-cart'></i> Add to Cart</button>\
+		 			</div>\
+		 		</div>\
+		 	</div>\
+		   </div>\
+		";
+
+		// append vào .html tương ứng
+		$(".section .container .row #main #store .row").append(divStr);
+	}
+   });
+
+});
