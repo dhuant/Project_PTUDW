adb9d7ff3ca7b9d08d395eb7e5d8b6567e206237
DESKTOP-G72575G\LINH DEP TRAI <nnlinh97it@gmail.com>
2018-04-17 22:34:25 +0700

    - update jquery index, link den nxb

---
+++ bookstore/assets/js/index.js
@@ -0,0 +1,596 @@
+var newBook = [
+    {
+        'url': 'newbook01.jpg',
+        'name': 'Bơ Đi Mà Sống',
+        'price': 75000,
+        'star': 5,
+        'status': 'new',
+        'sale': 35,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook02.jpg',
+        'name': 'Diệt Slime Suốt 300 Năm',
+        'price': 69000,
+        'star': 4,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook03.jpg',
+        'name': 'Đủ Xa Sẽ Cũ Đủ Lạ Sẽ Quên',
+        'price': 86000,
+        'star': 4,
+        'status': 'new',
+        'sale': 35,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook04.jpg',
+        'name': 'Hành Trình Của Alaina - Tập 1',
+        'price': 82000,
+        'star': 3,
+        'status': 'new',
+        'sale': 36,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook05.jpg',
+        'name': 'Cốt Cách Phụ Nữ',
+        'price': 86000,
+        'star': 5,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook06.jpg',
+        'name': 'Khéo Ăn Khéo Nói Sẽ Có Được Thiên Hạ',
+        'price': 88000,
+        'star': 4,
+        'status': 'new',
+        'sale': 40,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook07.jpg',
+        'name': 'Đắc Nhân Tâm',
+        'price': 82000,
+        'star': 4,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook08.jpg',
+        'name': 'Sherlock Homes Toàn Tập - Bộ 3 Cuốn',
+        'price': 88000,
+        'star': 4,
+        'status': 'new',
+        'sale': 30,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook09.jpg',
+        'name': 'Người Xưa Đã Quên Ngày Xưa',
+        'price': 92000,
+        'star': 3,
+        'status': 'new',
+        'sale': 36,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook10.jpg',
+        'name': 'Hôm Nay Tôi Thất Tình',
+        'price': 86000,
+        'star': 5,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook11.jpg',
+        'name': 'Nhớ Ai Đó Đến Kiệt Quệ',
+        'price': 89000,
+        'star': 5,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'newbook12.jpg',
+        'name': 'Nhớ Ai Đó Đến Kiệt Quệ',
+        'price': 92000,
+        'star': 3,
+        'status': 'new',
+        'sale': 40,
+        'nxb': '',
+        'category': 'newbook',
+        'view': 0,
+        'amount': 0
+    }
+];
+
+var bestSell = [
+    {
+        'url': 'best-sell-01.jpg',
+        'name': 'Pato Và Con Thú Mỏ Vịt',
+        'price': 90000,
+        'star': 4,
+        'status': 'new',
+        'sale': 30,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-02.jpg',
+        'name': 'Yêu Dấu',
+        'price': 110000,
+        'star': 5,
+        'status': '',
+        'sale': 35,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-03.jpg',
+        'name': 'Sóng Ngầm',
+        'price': 95000,
+        'star': 3,
+        'status': '',
+        'sale': 32,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-04.jpg',
+        'name': 'Hay Lời Tự Thú Của Một Copy-Writer',
+        'price': 85000,
+        'star': 5,
+        'status': 'new',
+        'sale': 35,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-05.jpg',
+        'name': 'Một Lít Nước Mắt',
+        'price': 100000,
+        'star': 3,
+        'status': 'new',
+        'sale': 40,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-06.jpg',
+        'name': 'Có Một Phố Vừa Đi Qua Phốt',
+        'price': 120000,
+        'star': 4,
+        'status': '',
+        'sale': 40,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-07.jpg',
+        'name': 'Thằng Nhóc',
+        'price': 110000,
+        'star': 4,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-08.png',
+        'name': 'Cơn Sốt Lúc Bình Minh',
+        'price': 80000,
+        'star': 5,
+        'status': '',
+        'sale': 32,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-09.jpg',
+        'name': 'Tên Tôi Là Đỏ',
+        'price': 135000,
+        'star': 4,
+        'status': 'new',
+        'sale': 40,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-10.jpg',
+        'name': 'Con Đường Hồi Giáo',
+        'price': 125000,
+        'star': 4,
+        'status': '',
+        'sale': 38,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-sell-11.jpg',
+        'name': 'Dưới Một Mái Nhà Ở Pari',
+        'price': 119000,
+        'star': 3,
+        'status': 'new',
+        'sale': 35,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+];
+
+var bestView = [
+    {
+        'url': 'best-view-01.jpg',
+        'name': 'Bơ Đi Mà Sống',
+        'price': 92000,
+        'star': 4,
+        'status': 'new',
+        'sale': 32,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-02.jpg',
+        'name': 'Diệt Slime Suốt 300 Năm',
+        'price': 85000,
+        'star': 4,
+        'status': 'new',
+        'sale': 30,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-03.jpg',
+        'name': 'Đủ Xa Sẽ Cũ Đủ Lạ Sẽ Quên',
+        'price': 90000,
+        'star': 4,
+        'status': 'new',
+        'sale': 32,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-04.jpg',
+        'name': 'Hành Trình Của Alaina - Tập 1',
+        'price': 120000,
+        'star': 4,
+        'status': '',
+        'sale': 40,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-05.jpg',
+        'name': 'Cốt Cách Phụ Nữ',
+        'price': 85000,
+        'star': 4,
+        'status': '',
+        'sale': 35,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-06.jpg',
+        'name': 'Có Một Phố Vừa Đi Qua Phố',
+        'price': 120000,
+        'star': 4,
+        'status': 'new',
+        'sale': 42,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-07.jpg',
+        'name': 'Thằng Nhóc',
+        'price': 88000,
+        'star': 4,
+        'status': 'new',
+        'sale': 37,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-08.png',
+        'name': 'Cơn Sốt Lúc Bình Minh',
+        'price': 95000,
+        'star': 4,
+        'status': 'new',
+        'sale': 35,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-09.jpg',
+        'name': 'Tên Tôi Là Đỏ',
+        'price': 82000,
+        'star': 4,
+        'status': '',
+        'sale': 32,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+    {
+        'url': 'best-view-10.jpg',
+        'name': 'Con Đường Hồi Giáo',
+        'price': 110000,
+        'star': 4,
+        'status': 'new',
+        'sale': 40,
+        'nxb': '',
+        'category': '',
+        'view': 0,
+        'amount': 0
+    },
+];
+
+var banner = [
+    'banner01.jpg',
+    'banner02.png',
+    'banner03.png',
+    'banner04.jpg',
+    'banner05.jpg',
+    'banner06.jpg',
+    'banner07.jpg'
+];
+
+var NXB = [
+    {
+        name: 'nxb01.jpg',
+        html: 'nhanam.html'
+    },
+    {
+        name: 'nxb02.jpg',
+        html: 'bachviet.html'
+    },
+    {
+        name: 'nxb03.jpg',
+        html: 'kimdong.html'
+    },
+    {
+        name: 'nxb04.jpg',
+        html: 'nxbtre.html'
+    },
+    {
+        name: 'nxb05.jpg',
+        html: 'bachviet.html'
+    },
+    {
+        name: 'nxb06.jpg',
+        html: 'nhanam.html'
+    },
+    {
+        name: 'nxb07.jpg',
+        html: 'nxbtre.html'
+    },
+    {
+        name: 'nxb08.jpg',
+        html: 'kimdong.html'
+    },
+    {
+        name: 'nxb09.jpg',
+        html: 'bachviet.html'
+    },
+    {
+        name: 'nxb10.jpg',
+        html: 'nxbtre.html'
+    },
+    {
+        name: 'nxb11.jpg',
+        html: 'kimdong.html'
+    },
+    {
+        name: 'nxb12.jpg',
+        html: 'nhanam.html'
+    }
+];
+
+for (var i = 0; i < newBook.length; i++) {
+    var strStar = '';
+    for (var j = 0; j < 5; j++) {
+        if (j < newBook[i].star) {
+            strStar += '<i class="fa fa-star"></i>';
+        }
+        else {
+            strStar += '<i class="fa fa-star-o empty"></i>';
+        }
+    }
+    $('#product-slick-1').append(`
+	<div class="product product-single">
+		<div class="product-thumb">
+                                    <div class="product-label">
+										<span class="sale">-${newBook[i].sale}%</span>
+                                        <span>New</span>
+									</div>
+									<img src="../assets/img/new-book/${newBook[i].url}" alt="">
+								</div>
+								<div class="product-body">
+									<h3 class="product-price"><a>${(newBook[i].price * (100 - newBook[i].sale) / 100).toLocaleString('de-DE')}đ </a><del class="product-old-price">${(newBook[i].price).toLocaleString('de-DE')}đ</del></h3>
+									<div class="product-rating">${strStar}</div>
+									<h2 class="product-name"><a href="#">${newBook[i].name}</a></h2>
+									<div class="product-btns">
+										<button class="main-btn icon-btn" title="Yêu thích"><i class="fa fa-heart"></i></button>
+										<button class="main-btn icon-btn" title="Compare"><i class="fa fa-exchange"></i></button>
+										<button class="primary-btn add-to-cart" title="Add to cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+									</div>
+								</div>
+							</div>
+    `);
+}
+
+for (var i = 0; i < bestSell.length; i++) {
+    var strStar = '';
+    var strNew = '';
+    for (var j = 0; j < 5; j++) {
+        if (j < bestSell[i].star) {
+            strStar += '<i class="fa fa-star"></i>';
+        }
+        else {
+            strStar += '<i class="fa fa-star-o empty"></i>';
+        }
+    }
+    if (bestSell[i].status === 'new') {
+        strNew += '<span>New</span>';
+    }
+    $('#product-slick-2').append(`
+	<div class="product product-single">
+		<div class="product-thumb">
+                                    <div class="product-label">
+                                        <span class="sale">-${bestSell[i].sale}%</span>
+                                        ${strNew}
+									</div>
+									<img src="../assets/img/best-sell/${bestSell[i].url}" alt="">
+								</div>
+								<div class="product-body">
+									<h3 class="product-price"><a>${(bestSell[i].price * (100 - bestSell[i].sale) / 100).toLocaleString('de-DE')}đ </a><del class="product-old-price">${(bestSell[i].price).toLocaleString('de-DE')}đ</del></h3>
+									<div class="product-rating">${strStar}</div>
+									<h2 class="product-name"><a href="#">${bestSell[i].name}</a></h2>
+									<div class="product-btns">
+										<button class="main-btn icon-btn" title="Yêu thích"><i class="fa fa-heart"></i></button>
+										<button class="main-btn icon-btn" title="Compare"><i class="fa fa-exchange"></i></button>
+										<button class="primary-btn add-to-cart" title="Add to cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+									</div>
+								</div>
+							</div>
+    `);
+}
+
+
+for (var i = 0; i < bestView.length; i++) {
+    var strStar = '';
+    var strNew = '';
+    for (var j = 0; j < 5; j++) {
+        if (j < bestView[i].star) {
+            strStar += '<i class="fa fa-star"></i>';
+        }
+        else {
+            strStar += '<i class="fa fa-star-o empty"></i>';
+        }
+    }
+    if (bestView[i].status === 'new') {
+        strNew += '<span>New</span>';
+    }
+    $('#product-slick-3').append(`
+	<div class="product product-single">
+		<div class="product-thumb">
+                                    <div class="product-label">
+										<span class="sale">-${bestView[i].sale}%</span>
+                                        ${strNew}
+									</div>
+									<img src="../assets/img/best-view/${bestView[i].url}" alt="">
+								</div>
+								<div class="product-body">
+									<h3 class="product-price"><a>${(bestView[i].price * (100 - bestView[i].sale) / 100).toLocaleString('de-DE')}đ </a><del class="product-old-price">${(bestView[i].price).toLocaleString('de-DE')}đ</del></h3>
+									<div class="product-rating">${strStar}</div>
+									<h2 class="product-name"><a href="#">${bestView[i].name}</a></h2>
+									<div class="product-btns">
+										<button class="main-btn icon-btn" title="Yêu thích"><i class="fa fa-heart"></i></button>
+										<button class="main-btn icon-btn" title="Compare"><i class="fa fa-exchange"></i></button>
+										<button class="primary-btn add-to-cart" title="Add to cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+									</div>
+								</div>
+							</div>
+    `);
+}
+
+
+for (var i = 0; i < banner.length; i++) {
+    $('#home-slick').append(`
+    <!-- banner -->
+					<div class="banner banner-1">
+						<img src="../assets/img/banner/${banner[i]}" alt="">
+					</div>
+	<!-- /banner -->
+    `);
+}
+
+for (var i = 0; i < NXB.length; i++) {
+    $('#nxb').append(`
+    <li>
+							<a href="../app/products/${NXB[i].html}">
+								<figure>
+									<img src="../assets//img/lblNXB/${NXB[i].name}" alt="">
+								</figure>
+							</a>
+	</li>
+    `)
+}
