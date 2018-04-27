74947afb0e9cd16c2bf72b0420f035da670460a9
TRANDINHHUAN <dinhhuantran3010@gmail.com>
2018-04-27 10:51:40 +0700

    fix thành công quản lý khách hàng

--- bookstore/app/admin/assets/js/khachhang/khachhang.js
+++ bookstore/app/admin/assets/js/khachhang/khachhang.js
@@ -1,83 +1,85 @@
 
-$(document).ready(function () {
-    var kh = [
-        {
-            hoten: 'Nguyễn Đình Tiến',
-            email: 'tien@gmail.com',
-            username: 'tiennd',
-            password: '*&.982j(^'
-        },
-        {
-            hoten: 'Trần Lê Thăng',
-            email: 'thang@gmail.com',
-            username: 'thangtl',
-            password: '!&#%2rkaf$%'
-        },
-        {
-            hoten: 'Hoàng Lê An',
-            email: 'an@gmail.com',
-            username: 'angl',
-            password: '&*@Hfaln.1a'
-        }
-    ];
 
-    /*tạo biến lưu Storege giống cookie */
-    var temp;
-    var titleHeader = [];
-    if (!sessionStorage.temp)
-        sessionStorage.temp = JSON.stringify(titleHeader);
-    titleHeader = JSON.parse(sessionStorage.temp);
+var kh = [
+    {
+        username: 'tiennd',
+        hoten: 'Nguyễn Đình Tiến',
+        email: 'tien@gmail.com',
+        password: '*&.982j(^'
+    },
+    {
+        username: 'thangtl',
+        hoten: 'Trần Lê Thăng',
+        email: 'thang@gmail.com',
+        password: '!&#%2rkaf$%'
+    },
+    {
+        username: 'angl',
+        hoten: 'Hoàng Lê An',
+        email: 'an@gmail.com',
+        password: '&*@Hfaln.1a'
+    }
+];
 
-    var check;
-    var Status = [];
-    if (!sessionStorage.check)
-        sessionStorage.check = JSON.stringify(Status);
-    Status = JSON.parse(sessionStorage.check);
-    /*end tạo biến lưu Storege giống cookie */
+/*tạo biến lưu Storege giống cookie */
+var temp;
+var khHeader = [];
+if (!sessionStorage.temp)
+    sessionStorage.temp = JSON.stringify(khHeader);
+khHeader = JSON.parse(sessionStorage.temp);
+
+var check;
+var Status = [];
+if (!sessionStorage.check)
+    sessionStorage.check = JSON.stringify(Status);
+Status = JSON.parse(sessionStorage.check);
+/*end tạo biến lưu Storege giống cookie */
 
 
-    for (i = 0; i < kh.length; i++) {
-        var idtr = `tr${kh[i].hoten}`;
-        var idBtnDel = `delete${kh[i].hoten}`;
-        var idBtnEdit = `edit${kh[i].hoten}`;
-        $('#kh').append(`
-        <tr id="${idtr}">
+for (i = 0; i < kh.length; i++) {
+    var idtr = `tr${kh[i].username}`;
+    var idDel = `delete${kh[i].username}`;
+    var idEdit = `edit${kh[i].username}`;
+    $('#kh').append(`
+        <tr id="${idtr}">   
         <td>${i + 1}</td>
+        <td>${kh[i].username}</td>
         <td>${kh[i].hoten}</td>
         <td>${kh[i].email}</td>
-        <td>${kh[i].username}</td>
         <td>${kh[i].password}</td>
         <td>
             <div class="hidden-sm hidden-xs btn-group">
-                <a id="${idBtnEdit}" href="./detail-khachhang.html" class="btn btn-xs btn-info">
+                <a id="${idEdit}" href="./detail-khachhang.html" class="btn btn-xs btn-info">
                     <i class="ace-icon fa fa-pencil bigger-120"></i>
                 </a>
             </div>
         </td>
         <td>
             <div class="hidden-sm hidden-xs btn-group">
-                <button id="${idBtnDel}" class="btn btn-xs btn-danger">
+                <button id="${idDel}" class="btn btn-xs btn-danger">
                     <i class="ace-icon fa fa-trash bigger-120"></i>
                 </button>
             </div>
         </td>
          </tr>
         `);
-    }
-    /*nút xóa trong khách hàng */
-    for (var j = 0; j < kh.length; j++) {
-        var jq = `#delete${kh[j].hoten}`;
-        $(jq).on('click', function () {
-            var id = $(this).attr('id');
-            for (var k = 0; k < kh.length; k++) {
-                var idBtnDel = `delete${kh[k].hoten}`;
-                if (idBtnDel === id) {
-                    var idtr = `#tr${kh[k].hoten}`;
-                    $(idtr).remove();
-                    $('body').append(`
+}
+/*nút xóa trong khách hàng */
+for (var j = 0; j < kh.length; j++) {
+    var jq = `#delete${kh[j].username}`;
+    $(jq).on('click', function () {
+        var id = $(this).attr('id');
+        for (var k = 0; k < kh.length; k++) {
+            var idBtnDel = `delete${kh[k].username}`;
+            if (idBtnDel === id) {
+                var idtr = `#tr${kh[k].username}`;
+                $(idtr).remove();
+
+                /*thông báo delete successfully */
+                $('body').append(`
                 <div class="alert alert-success" id="delete" 
                 style="
-                        position: fixed;
+                         position: fixed;
                         top: 20px;
                         right: 20px;
                         z-index: 99999;
@@ -85,52 +87,53 @@ $(document).ready(function () {
                         ">
                         <i class="fa fa-check">
                         </i> Delete Successfully </div>`);
-                    $('#delete').fadeIn('slow');
+                $('#delete').fadeIn('slow');
+                setTimeout(() => {
+                    $('#delete').fadeOut(3500);
                     setTimeout(() => {
-                        $('#delete').fadeOut(2500);
-                        setTimeout(() => {
-                            $('#delete').remove();
-                        }, 1500);
-                    }, 1000);
-                }
+                        $('#delete').remove();
+                    }, 1500);
+                }, 500);
+                /*end thông báo delete successfully */
+
             }
-        });
-
-    }
-    /* end nút xóa trong loại sách */
-
-    /*push vào trong Storage */
-    for (var j = 0; j < kh.length; j++) {
-        var jq = `#edit${kh[j].hoten}`;
-        $(jq).on('click', function () {
-            titleHeader = [];
-            var id = $(this).attr('id');
-            for (var k = 0; k < kh.length; k++) {
-                var idEdit = `edit${kh[k].hoten}`;
-                if (idEdit === id) {
-                    var str = 'Chỉnh sửa thông tin khách hàng';
-                    titleHeader.push(str);
-                    break;
-                }
-            }
-            sessionStorage.temp = JSON.stringify(titleHeader);
-            titleHeader = JSON.parse(sessionStorage.temp);
-        });
-    }
-    $('#titleHeader').append(`
-        <h4 class="panel-title" style="padding-top: 12px;">${titleHeader[0]}</h4>
-    `);
-    /*end push vào trong Storage */
-
-    /*end set lại cho input bên thêm mới kh */
-
-    $('#save-them-kh').click(function () {
-        Status = [];
-        Status.push('save-kh-success');
-        sessionStorage.check = JSON.stringify(Status);
-        Status = JSON.parse(sessionStorage.check);
+        }
     });
-    $('body').append(`
+
+}
+/* end nút xóa trong loại sách */
+
+for (var j = 0; j < kh.length; j++) {
+    var jq = `#edit${kh[j].username}`;
+    $(jq).on('click', function () {
+        khHeader = [];
+        var id = $(this).attr('id');
+        for (var k = 0; k < kh.length; k++) {
+            var idEdit = `edit${kh[k].username}`;
+            if (idEdit === id) {
+                var str = 'Chỉnh sửa thông tin khách hàng';
+                khHeader.push(str);
+                break;
+            }
+        }
+        sessionStorage.temp = JSON.stringify(khHeader);
+        khHeader = JSON.parse(sessionStorage.temp);
+    });
+}
+$('#kh-Header').append(`
+        <h4 class="panel-title" style="padding-top: 12px;">${khHeader[0]}</h4>
+    `);
+/*end push vào trong Storage */
+
+/*end set lại cho input bên thêm mới kh */
+
+$('#save-them-kh').click(function () {
+    Status = [];
+    Status.push('save-kh-success');
+    sessionStorage.check = JSON.stringify(Status);
+    Status = JSON.parse(sessionStorage.check);
+});
+$('body').append(`
     <div class="alert alert-success" id="${Status[0]}" 
     style="
             position: fixed;
@@ -142,16 +145,13 @@ $(document).ready(function () {
             <i class="fa fa-check">
             </i> Save Successfully </div>
     `);
-    $('#save-kh-success').fadeIn('slow');
+$('#save-kh-success').fadeIn('slow');
+setTimeout(() => {
+    $('#save-kh-success').fadeOut(2500);
     setTimeout(() => {
-        $('#save-kh-success').fadeOut(2500);
-        setTimeout(() => {
-            $('#save-kh-success').remove();
-        }, 1500);
-    }, 1000);
-    Status = [];
-    sessionStorage.check = JSON.stringify(Status);
-    Status = JSON.parse(sessionStorage.check);
-});
-
-
+        $('#save-kh-success').remove();
+    }, 1500);
+}, 1000);
+Status = [];
+sessionStorage.check = JSON.stringify(Status);
+Status = JSON.parse(sessionStorage.check);
